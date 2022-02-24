6b5cc78ee5442fac7dc1e986646725b9a9ffb3a3
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2022-02-24 01:44:49 -0500

    Worked on Image Processing

--- image_processor.py
+++ image_processor.py
@@ -1,7 +1,7 @@
 import skimage
 from skimage import color
 from skimage import io
-import numpy as numpy
+import numpy as np
 import time
 import cv2
 from statistics import mean, stdev
@@ -20,40 +20,44 @@ class image_processor:
 
     def newImage(self, image_title):
         self.imgTitle = image_title
-        print("imported Image Title =" + self.imgTitle + " of type " + str(type(self.imgTitle)))
+        print("imported Image Title = " + self.imgTitle + " ----- of type " + str(type(self.imgTitle)))
 
-    def imageResizeBW(self):
-        img = cv2.imread(self.imgTitle)
-        print("img after cv2.read = " + str(type(img)))
-        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        resized = cv2.resize(imgGray, (self.dispWidth, self.dispHeight), interpolation = cv2.INTER_AREA)
-        return resized
-    
     def pixelAverageVal(self, downsized):
         sum = 0
+        print(downsized.shape)
         h, w = downsized.shape
         for y in range(0,h):
             for x in range(0,w):
                 sum += downsized[y][x]
         return sum / (h * w)
 
-    def imgToDisp(self, image_title):
-        self.newImage(image_title)
-        r = self.imageResizeBW()
-        print('plotting')
-        print('pixel average ' + str(mean(r)))
-        plt.imshow(r, cmap='gray')
+    def displayRGB(self):
+        r = self.imageResizeRGB()
+        plt.imshow(r)
         plt.show()
-        return 1
-        #print(self.pixelAverageVal(resized))
+
+    def imageResizeRGB(self):
+        img = cv2.imread(self.imgTitle)
+        resized = cv2.resize(img, (self.dispWidth, self.dispHeight), interpolation = cv2.INTER_AREA)
+        return resized
+
+    def displayBW(self):
+        r = self.imageResizeBW()
+        plt.imshow(r, cmap = "gray")
+        plt.show()
+
+    def imageResizeBW(self):
+        img = cv2.imread(self.imgTitle)
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        resized = cv2.resize(imgGray, (self.dispWidth, self.dispHeight), interpolation = cv2.INTER_AREA)
+        return resized
 
 if __name__ == '__main__':
     ip = image_processor(('#CD853F','#8B5A2B','#008080','#D8BFD8'), 16, 16)
-    r = ip.imgToDisp('Ideas_Surprised_Pikachu_HD.jpg')
-    print(r)
+    ip.newImage('Ideas_Surprised_Pikachu_HD.jpg')
+    ip.displayRGB()
+
     i = 0
     while True:
         time.sleep(1)
         i += 1
-
-    
---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2 as cv
+img = cv.imread('Ideas_Surprised_Pikachu_HD.jpg')
+Z = img.reshape((-1,3))
+# convert to np.float32
+Z = np.float32(Z)
+# define criteria, number of clusters(K) and apply kmeans()
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+K = 4
+ret,label,center=cv.kmeans(Z,K,None,criteria,10,cv.KMEANS_RANDOM_CENTERS)
+# Now convert back into uint8, and make original image
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+cv.imshow('res2',res2)
+cv.waitKey(0)
+cv.destroyAllWindows()
