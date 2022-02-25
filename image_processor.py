7e1d64c98bd404e016c67bd586e508851d089069
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2022-02-24 19:48:42 -0500

    color simplification function progress
    implemented kmeans

--- image_processor.py
+++ image_processor.py
@@ -52,12 +52,32 @@ class image_processor:
         resized = cv2.resize(imgGray, (self.dispWidth, self.dispHeight), interpolation = cv2.INTER_AREA)
         return resized
 
-if __name__ == '__main__':
-    ip = image_processor(('#CD853F','#8B5A2B','#008080','#D8BFD8'), 16, 16)
-    ip.newImage('Ideas_Surprised_Pikachu_HD.jpg')
-    ip.displayRGB()
+    def reduceColors(self, img, K):
+        Z = img.reshape((-1,3))
 
+        # convert to np.float32
+        Z = np.float32(Z)
+
+        # define criteria, number of clusters(K) and apply kmeans()
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+        # Now convert back into uint8, and make original image
+        center = np.uint8(center)
+        res = center[label.flatten()]
+        res2 = res.reshape((img.shape))
+        cv2.imshow('res2',res2)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    ip = image_processor(('#CD853F','#8B5A2B','#008080','#D8BFD8'), 32, 32)
+    ip.newImage('Ideas_Surprised_Pikachu_HD.jpg')
+    ## ip.displayBW()
+    ip.reduceColors(ip.imageResizeRGB(), 4)
     i = 0
     while True:
         time.sleep(1)
         i += 1
+
+    
