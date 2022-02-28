bafb2ea0c2a799d6354175f9fb6e79ff36aa4c30
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2022-02-28 12:46:07 -0500

    created differentTime

---
+++ different_time.py
@@ -0,0 +1,40 @@
+from zoneinfo import ZoneInfo, available_timezones
+from datetime import datetime, timedelta
+import time
+import copy
+
+
+class different_time:
+    def __init__(self):
+        self.then = copy.deepcopy(time.localtime())
+
+    def getThen(self):
+        return self.then
+
+    def beenSecond(self):
+        if self.then.tm_sec is time.localtime().tm_sec:
+            return False
+        else:
+            self.then = copy.deepcopy(time.localtime())
+            return True
+    
+    def beenMinute(self):
+        if self.then.tm_min is time.localtime().tm_min:
+            return False
+        else:
+            self.then = copy.deepcopy(time.localtime())
+            return True
+    
+    def beenHour(self):
+        if self.then.tm_hour is time.localtime().tm_hour:
+            return False
+        else:
+            self.then = copy.deepcopy(time.localtime())
+            return True
+
+if __name__ == '__main__':
+    dt = different_time()
+
+    while True:
+        if dt.beenMinute():
+            print(dt.getThen().tm_min)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from disp_def import DispDef, blockStateKey
 import animation as ani
 import image_processor as impr
 import display_virtual as disvir
+import different_time
 
 dispDim = (6, 6) # (width, height)
 pixelVal = ('#CD853F','#8B5A2B','#008080','#D8BFD8')
@@ -11,6 +12,15 @@ displayTit = '6x9 test'
 a = ani.animation()
 ip = impr.image_processor(pixelVal, dispDim)
 disp = disvir.display_virtual(displayTit, dispDim, DispDef.TOP, DispDef.RIGHT, pixelVal)  
+now = datetime.now()
+
+then = 0
+while True:
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print(current_time)
+    time.sleep(1)
+
 
 #copmiles till here
 
