7ef7cb5f3185b4da0bd28c346bc89774964c3c3b
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2022-03-13 01:42:11 -0500

    Update timer.py

--- timer.py
+++ timer.py
@@ -10,33 +10,46 @@ class timer:
     def __init__(self):
         now = dt.datetime.now()
         self.tz = now.astimezone()
-        self.resetThen()
+        self.__resetThen()
+
+    def getNow(self):
+        return (self.getHour() * 100 + self.getMin())
+
+    def getHour(self):
+        now = dt.datetime.now()
+        hour = now.hour
+        if hour == 0:
+            hour = 12
+        return hour
+
+    def getMin(self):
+        return dt.datetime.now().minute
 
     def getThen(self):
         return self.then
 
-    def resetThen(self):
+    def __resetThen(self):
         self.then = copy.deepcopy(dt.datetime.now())
 
     def beenSecond(self):
         if self.getThen().second is dt.datetime.now().second:
             return False
         else:
-            self.resetThen()
+            self.__resetThen()
             return True
     
     def beenMinute(self):
         if self.getThen().minute is dt.datetime.now().minute:
             return False
         else:
-            self.resetThen()
+            self.__resetThen()
             return True
     
     def beenHour(self):
         if self.getThen().hour is dt.datetime.now().hour:
             return False
         else:
-            self.resetThen()
+            self.__resetThen()
             return True
 
     def beenXmils(self, x):
@@ -44,13 +57,11 @@ class timer:
         if waitTill > dt.datetime.now():
             return False
         else:
-            self.resetThen()
-            return True
-        
+            self.__resetThen()
+            return True      
 
 if __name__ == '__main__':
     t = timer()
-
     while True:
-        if t.beenXmils(1750):
-            print(t.getThen())
+        if t.beenMinute():
+            print(t.getNow())
